21f5346032a4f0d65b9ac5c02dd618522920c35b
mest0606 <mest.yalcin@gmail.com>
2022-07-18 08:14:38 +0300

    First commit (kodluyoruz

---
+++ src/test/java/com/mesutyalcin/KodluyoruzSpringBoot/KodluyoruzSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mesutyalcin.KodluyoruzSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
